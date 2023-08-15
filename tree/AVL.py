b8054d3125284757797c38ed47f6a59fb9df7da5
Vedant Pandey <vedantpandey459@gmail.com>
2023-08-16 01:00:46 +0530

    avl

---
+++ tree/AVL.py
@@ -0,0 +1,58 @@
+from Queue import Queue2 as queue
+
+class AVLNode:
+    def __init__(self,data):
+        self.data=data
+        self.leftChild=None
+        self.rightChild=None
+        self.height=1
+    
+def preOrderTraversal(rootNode):
+    if not rootNode:
+        return 
+    print(rootNode.data)
+    preOrderTraversal(rootNode.leftChild)
+    preOrderTraversal(rootNode.rightChild)
+
+def inOrderTraversal(rootNode):
+    if not rootNode:
+        return 
+    inOrderTraversal(rootNode.leftChild)
+    print(rootNode.data)
+    inOrderTraversal(rootNode.rightChild)
+
+def postOrderTraversal(rootNode):
+    if not rootNode:
+        return 
+    postOrderTraversal(rootNode.leftChild)
+    postOrderTraversal(rootNode.rightChild)
+    print(rootNode.data)
+
+def levelOrderTraversal(rootNode):
+    if not rootNode:
+        return 
+    else:
+        customQueue=queue.Queue()
+        customQueue.enqueue(rootNode)
+        while not(customQueue.isEmpty()):
+            root=customQueue.dequeue()
+            print(root.value.data)
+            if root.value.leftChild is not None:
+                customQueue.enqueue(root.value.leftChild)
+            if root.value.rightChild is not None:
+                customQueue.enqueue(root.value.rightChild)
+
+def searchNode(rootNode,nodeValue):
+    if rootNode.data==nodeValue:
+        print("The value is found")
+    elif nodeValue<rootNode.data:
+        if rootNode.leftChild.data==nodeValue:
+            print("The value is found")
+        else:
+            searchNode(rootNode.leftChild,nodeValue)
+    else:
+        if rootNode.rightChild.data==nodeValue:
+            print("The value is found")
+        else:
+            searchNode(rootNode.rightChild,nodeValue)
+
