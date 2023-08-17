5814c99f4958fc15ba4f07a098ffb6566002144b
Vedant Pandey <vedantpandey459@gmail.com>
2023-08-17 23:52:22 +0530

    heap trie

--- tree/BinaryHeap.py
+++ tree/BinaryHeap.py
@@ -47,3 +47,43 @@ def insertNode(rootNode,nodeValue,heapType):
     rootNode.heapSize+=1
     heapifyTreeInsert(rootNode,rootNode.heapSize,heapType)
     return "The value has been successfullt inserted"
+
+def heapifyTreeExtract(rootNode,index,heapType):
+    leftIndex=index*2
+    rightIndex=index*2+1
+    swapChild=0
+
+    if rootNode.heapSize<leftIndex:
+        return 
+    elif rootNode.heapSize==leftIndex:
+        if heapType=="Min":
+            if rootNode.customList[index]>rootNode.customList[leftIndex]:
+                rootNode.customList[index],rootNode.customList[leftIndex]=rootNode.customList[leftIndex],rootNode.customList[index]
+                return
+            else:
+                if rootNode.customList[index]<rootNode.customList[leftIndex]:
+                        rootNode.customList[index],rootNode.customList[leftIndex]=rootNode.customList[leftIndex],rootNode.customList[index]
+                        return
+    else:
+        if heapType=="Min":
+            if rootNode.customList[leftIndex]<rootNode.customList[rightIndex]:
+                swapChild=leftIndex
+            else:
+                swapChild=rightIndex
+            if rootNode.customList[index]>rootNode.customList[swapChild]:
+                rootNode.customList[index],rootNode.customList[swapChild]=rootNode.customList[swapChild],rootNode.customList[index]
+    heapifyTreeExtract(rootNode,swapChild,heapType)
+
+def extractNode(rootNode,heapType):
+    if rootNode.heapSize==0:
+        return
+    else:
+        extractedNode=rootNode.customList[1]
+        rootNode.customList[1]=rootNode.customList[rootNode.heapSize]
+        rootNode.customList[rootNode.heapSize]=None
+        rootNode.heapSize-=1
+        heapifyTreeExtract(rootNode,1,heapType)
+        return extractedNode
+    
+def delete(rootNode):
+    rootNode.customList=None
